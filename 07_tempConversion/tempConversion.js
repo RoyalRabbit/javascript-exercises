60630c8ca90bfd004b66e2e88166b9776e9129d6
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-15 15:06:06 -0400

    Finish 06-07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+let cel = ((deg-32)*5/9);
+cel = parseFloat((Math.round(cel * 10) / 10).toFixed(1));
+return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+let fah = (deg * 9 / 5 + 32);
+fah = parseFloat((Math.round(fah*10)/10).toFixed(1));
+return fah;
 };
 
 // Do not edit below this line
