867fa88f4aeebca801c4169155ef18b1a8756c91
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-27 11:48:42 -0400

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let accumulator=0;
+	numbers.forEach(number=>accumulator+=number);
+  return accumulator;
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+	let accumulator=numbers[0];
+	numbers.forEach((number, index)=>{
+    if (index === 0) return;
+    accumulator -= number;
+    
+  })
+  return accumulator;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator,current)=>accumulator+=current,0);
+  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, current)=>accumulator*=current);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	let total=number;
+  for (let i=1; i<power; i++) {;
+    total *= number;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a===0) return 1;
+  return a*factorial(a-1);
+
 };
 
 // Do not edit below this line
