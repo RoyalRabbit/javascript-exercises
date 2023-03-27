9a819ebc046e7f02f99083a9c5157942a4f9d088
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-27 13:25:28 -0400

    Finish fibonacci using recursive function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    const num = Number.parseFloat(arg);
+    if (num < 1) return ('OOPS')
+    return fib(num)
 
 };
 
+function fib(num) {
+    if (num === 1) return 1
+    if (num === 2) return 1
+    return fib(num-1)+fib(num-2)
+}
 // Do not edit below this line
 module.exports = fibonacci;
