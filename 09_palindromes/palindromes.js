46cf74483a96767cba6032cde3ccf7ef346b92f2
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-27 12:58:18 -0400

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const filtered = str.replace(/\s/g,'').replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    const reverseArray = filtered.split('').reverse().join('');
+    return filtered === reverseArray;
 };
 
 // Do not edit below this line
