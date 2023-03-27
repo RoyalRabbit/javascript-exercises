6bb870f1559f5e4a24a4ed4d10fb6c909d1bc6f0
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-27 13:26:42 -0400

    Fix some error checking

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,14 @@
 const fibonacci = function(arg) {
     const num = Number.parseFloat(arg);
-    if (num < 1) return ('OOPS')
+    if (num < 0) return ('OOPS')
     return fib(num)
 
 };
 
 function fib(num) {
+    if (num === 0) return 0
     if (num === 1) return 1
-    if (num === 2) return 1
+    
     return fib(num-1)+fib(num-2)
 }
 // Do not edit below this line
