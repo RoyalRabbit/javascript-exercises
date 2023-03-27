fbaac957db8cf48137c283410b2dcb54e03d4ab3
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-27 14:04:34 -0400

    Passed findTheOldest test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.forEach(obj=>{
+        if (!obj.yearOfDeath) obj.yearOfDeath = new Date().getFullYear()
+    })
+    const oldest = array.reduce((accumulator, current)=>{
+        const currentAge = current.yearOfDeath - current.yearOfBirth;
+        const oldestAge = accumulator.yearOfDeath - accumulator.yearOfBirth;
+        return currentAge > oldestAge ? current : accumulator;
+    })
+    return oldest;
 };
 
 // Do not edit below this line
